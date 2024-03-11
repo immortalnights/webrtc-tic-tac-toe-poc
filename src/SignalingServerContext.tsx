5bc1e5de62bdc6f8f69cafd0bdee60b1361fee1d
Andrew <github@seventh.space>
2024-03-11 23:39:08 +0000

    fix: fixes and improvements to SignalingServerContext

--- src/SignalingServerContext.tsx
+++ src/SignalingServerContext.tsx
@@ -1,32 +1,66 @@
-import { ReactNode, createContext, useCallback, useMemo, useState } from "react"
+import {
+    ReactNode,
+    createContext,
+    useCallback,
+    useContext,
+    useMemo,
+    useRef,
+    useState,
+} from "react"
 import { SignalingServerConnection } from "./multiplayer-lib/SignalingServerConnection"
-import { GameOptions, PlayerRecord, RoomRecord } from "game-signaling-server"
+import {
+    GameOptions,
+    PlayerRecord,
+    RoomRecord,
+    ServerReplyData,
+    throwError,
+} from "game-signaling-server"
+import { PeerConnectionContext } from "./PeerConnectionContext"
 
 export interface SignalingServerContextValue {
     connected: boolean
-    ws: SignalingServerConnection | null
-    player: { id: string; name: string } | null
-    rooms: RoomRecord[]
-    setRooms: React.Dispatch<React.SetStateAction<RoomRecord[]>> | undefined
-    connect: () => void
-    disconnect: () => void
-    host: (name: string, options: GameOptions) => void
-    join: (room: RoomRecord) => void
+    connect: (name: string) => Promise<PlayerRecord>
+    disconnect: () => Promise<void>
+    getGames: () => Promise<{ games: RoomRecord[] }>
+    host: (name: string, options: GameOptions) => Promise<RoomRecord>
+    join: (room: RoomRecord) => Promise<RoomRecord>
+    setReady: (id: string, ready: boolean) => void
+    start: (id: string) => void
+    leave: () => void
+    exit: () => void
+    subscribe: SignalingServerConnection["subscribe2"]
+    unsubscribe: SignalingServerConnection["unsubscribe"]
 }
 
 export const SignalingServerContext =
     createContext<SignalingServerContextValue>({
         connected: false,
-        ws: null,
-        player: null,
-        rooms: [],
-        setRooms: undefined,
-        connect: () =>
-            console.error("Call to default Context connect function"),
-        disconnect: () =>
-            console.error("Call to default Context disconnect function"),
-        host: () => console.error("Call to default Context host function"),
-        join: () => console.error("Call to default Context join function"),
+        connect: () => {
+            console.error("Call to default Context connect function")
+            return Promise.reject()
+        },
+        disconnect: () => {
+            console.error("Call to default Context disconnect function")
+            return Promise.reject()
+        },
+        host: () => {
+            console.error("Call to default Context host function")
+            return Promise.reject()
+        },
+        join: () => {
+            console.error("Call to default Context join function")
+            return Promise.reject()
+        },
+        getGames: () => {
+            console.error("Call to default Context getGames function")
+            return Promise.reject()
+        },
+        setReady: () => {},
+        start: () => {},
+        leave: () => {},
+        exit: () => {},
+        subscribe: () => console.error(""),
+        unsubscribe: () => console.error(""),
     })
 
 const SignalingServerContextProvider = ({
@@ -36,75 +70,169 @@ const SignalingServerContextProvider = ({
     address?: string
     children: ReactNode
 }) => {
-    const [player, setPlayer] = useState<PlayerRecord>({
-        id: "",
-        name: "BrowserPlayer",
-        ready: false,
-        host: false,
-    })
+    const { offer, answer } = useContext(PeerConnectionContext)
     const [connected, setConnected] = useState(false)
-    const [ws] = useState(
+    const ws = useRef(
         new SignalingServerConnection(address ?? "127.0.0.1:9001"),
     )
-    const [rooms, setRooms] = useState<RoomRecord[]>([])
 
-    const connect = useCallback(async () => {
-        try {
+    const connect = useCallback(
+        async (name: string) => {
             console.log("Connecting...")
-            await ws.connect()
+            await ws.current.connect()
             console.log("Connected")
 
-            ws.send("player-join-lobby", { name: player.name })
-            const resp = await ws.waitForMessage<PlayerRecord>(
+            ws.current.send("player-join-lobby", { name })
+            const resp = await ws.current.waitForMessage<PlayerRecord>(
                 "player-join-lobby-reply",
                 5000,
             )
 
-            setPlayer((p) => ({ ...p, id: resp?.id }))
+            if (!resp) {
+                throw new Error("Invalid connection reply")
+            }
+
             setConnected(true)
-        } catch (error) {
-            console.error("Failed to connect", error)
-        } finally {
-            console.log("Connection completed")
-        }
-    }, [ws, player.name])
-
-    const disconnect = useCallback(async () => {
-        console.log("Disconnect")
-        if (ws.connected) {
-            console.log("Disconnecting...")
-            await ws.disconnect()
-            console.log("Disconnected")
-        }
-        setRooms([])
-        setConnected(false)
-        setPlayer((p) => ({ ...p, ready: false, host: false }))
-    }, [ws])
-
-    // useEffect(() => {
-
-    const host = useCallback(
-        async (name: string, options: GameOptions) => {},
+            return resp
+        },
         [ws],
     )
 
-    const join = useCallback(async (room: RoomRecord) => {}, [ws])
+    const disconnect = useCallback(async () => {
+        console.log("Disconnect")
+        if (ws.current.connected) {
+            console.log("Disconnecting...")
+            await ws.current.disconnect()
+            console.log("Disconnected")
+        }
+        setConnected(false)
+    }, [ws])
 
-    //     console.log("connection status changed", ws.connected)
-    // }, [ws.connected])
+    const getGames = useCallback(async () => {
+        console.log("Requesting games")
+
+        ws.current.send("player-list-games")
+
+        type ReplyMessageData = ServerReplyData<"player-list-games-reply">
+
+        const resp = (await ws.current.waitForMessage<ReplyMessageData>(
+            "player-list-games-reply",
+        )) as unknown as { games: RoomRecord[] }
+
+        return resp
+    }, [ws])
+
+    const host = useCallback(
+        async (name: string, options: GameOptions) => {
+            console.log("Requesting Peer offer")
+            const { offer: sessionDescription, iceCandidates } = await offer()
+
+            console.log("Sending host game message")
+            ws.current.send("player-host-game", {
+                name,
+                options,
+                sessionDescription,
+                iceCandidates,
+            })
+
+            type ReplyMessageData = ServerReplyData<"player-host-game-reply">
+
+            console.log("Waiting for host game response")
+            const data = (await ws.current.waitForMessage<ReplyMessageData>(
+                "player-host-game-reply",
+            )) as unknown as RoomRecord
+
+            return data
+        },
+        [ws, offer],
+    )
+
+    const join = useCallback(
+        async (room: RoomRecord) => {
+            const host =
+                room.players.find((player) => player.host) ??
+                throwError("Failed to find host")
+
+            if (!host.sessionDescription) {
+                throw Error("Room host does not have peer connection")
+            }
+
+            if (!host.iceCandidates) {
+                throw Error("Room host does not have ice candidates")
+            }
+
+            console.debug("Answering RTC offer")
+            const sessionDescription = await answer(
+                host.sessionDescription,
+                host.iceCandidates,
+            )
+
+            ws.current.send("player-join-game", {
+                id: room.id,
+                sessionDescription,
+            })
+
+            type ReplyMessageData = ServerReplyData<"player-join-game-reply">
+
+            const data = (await ws.current.waitForMessage<ReplyMessageData>(
+                "player-join-game-reply",
+            )) as unknown as RoomRecord
+
+            return data
+        },
+        [ws, answer],
+    )
+
+    const setReady = useCallback(
+        (id: string, ready: boolean) => {
+            ws.current.send("player-change-ready-state", { id, ready })
+        },
+        [ws],
+    )
+
+    const start = useCallback(
+        (id: string) => {
+            ws.current.send("player-start-game", { id })
+        },
+        [ws],
+    )
+
+    const leave = useCallback(() => {
+        ws.current.send("player-leave-game")
+    }, [ws])
+
+    const exit = useCallback(() => {
+        ws.current.send("player-leave-lobby")
+    }, [ws])
+
     const value = useMemo(
         () => ({
-            player,
             connected,
-            ws,
-            rooms,
-            setRooms,
             connect,
             disconnect,
+            getGames,
             host,
             join,
+            setReady,
+            start,
+            leave,
+            exit,
+            subscribe: ws.current.subscribe2.bind(ws.current),
+            unsubscribe: ws.current.unsubscribe.bind(ws.current),
         }),
-        [player, connected, ws, rooms, connect, disconnect, host, join],
+        [
+            connected,
+            ws,
+            connect,
+            disconnect,
+            getGames,
+            host,
+            join,
+            setReady,
+            start,
+            leave,
+            exit,
+        ],
     )
 
     return (
